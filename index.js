f884e8997f5a421795f4ecf0225f86aa09fd62f3
Cosmic_Crafter <138387813+Cosmic-Crafter-1@users.noreply.github.com>
2024-06-03 00:36:45 +0530

    Updated to try-catch and module-async-await. A modern approach.

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+    document.getElementById("author").textContent = `By: Author name.`   
+}
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
+// With the try block, I'm able to use the "res" variable twice, since it's block scoped.
+
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    const data = await res.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+    /**    <p>🎯: $${data.market_data.current_price.usd}</p>
+    /**    <p>👆: $${data.market_data.high_24h.usd}</p>
+    /**    <p>👇: $${data.market_data.low_24h.usd}</p>
+`    
+    } catch(error) {
+        console.log(err)
+}
+
 
 function getCurrentTime() {
     const date = new Date()
@@ -38,57 +38,20 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
-
-
-
-
-
-
-
-
-
-if("geolocation" in navigator) {
-    console.log("The (navigator.geolocation) object exists, Geolocation services available. ")
-} else {
-    console.log("The object doesn't exists, geolocation services not available.")
-}
-
-// The method takes in 2 callback functions as parameter, with optional 3rd parameter.
-navigator.geolocation.getCurrentPosition(success, error)
-
-function success(position) {
-    const coordinates = position.coords;
-    const latitude = coordinates.latitude
-    const longitude = coordinates.longitude
-    console.log("Your coordinates are: " + coordinates)
-    console.log("Latitude: " + latitude)
-    console.log("Longitude: " + longitude)
-    console.log(`More or Less: ${coordinates.accuracy} metres.`)
-    
-    console.log("----------------------------------------")
-    console.log("API info: ")
-    
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`)
-        .then(response => {
-            if(!response.ok) {
-                throw Error("Weather data currently unavailable.")
-            }
-           return response.json()
-        })
-        .then(data => {
-            console.log(data)
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            console.log("Icon URL: " + iconUrl)
-            document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `
-                console.log("----------------------------------------")
-        })
-        .catch(err => console.log("Error: " + err))
-}
-
-function error(err) {
-    console.log("An error occured: " + err)
-}
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        const data = await res.json()
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `        
+        } catch(err) {
+            console.error(err)
+    }
+});
